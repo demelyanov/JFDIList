5760ae42f55322a40bd276b0a8f1eccf81ccd608
Dmitry Emelyanov <mvcdeveloper@gmail.com>
2019-02-28 18:42:11 +0300

    Model update

---
+++ backend/Domain/Model/TaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace JFDIList.Domain.Model
+{
+    public class TaskList
+    {
+        [ForeignKey("TaskItem")]
+        public int TaskId { get; set; }
+        public TaskItem Task { get; set; }
+        public int ListId { get; set; }
+        public List List { get; set; }
+    }
+}
---
+++ backend/Domain/Model/TaskTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace JFDIList.Domain.Model
+{
+    public class TaskTag
+    {
+        [ForeignKey("TaskItem")]
+        public int TaskId { get; set; }
+        public TaskItem Task { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
