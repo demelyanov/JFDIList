08f582c1ffe626034fe57e2d06f8bcbcced17524
Dmitry Emelyanov <mvcdeveloper@gmail.com>
2019-03-23 14:15:50 +0300

    Structure update

---
+++ backend/dotnetcore2.2/DataAccess/Repositories/TaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using JFDIList.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JFDIList.DataAccess.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+    }
+}
